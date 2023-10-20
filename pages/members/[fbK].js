83ddca166329fd19ddcf7341728e3cb27ab601e2
Alex Sendre <rares.sendre@gmail.com>
2023-10-20 15:14:13 -0500

    fixed member view to include a bit of information

--- pages/members/[fbK].js
+++ pages/members/[fbK].js
@@ -1,6 +1,7 @@
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
-
+import { Button } from 'react-bootstrap';
+import Link from 'next/link';
 import { viewMemberDetails } from '../../api/mergedData';
 
 export default function ViewSingleMember() {
@@ -21,13 +22,18 @@ export default function ViewSingleMember() {
       <div className="mt-5 d-flex flex-wrap">
         <div className="d-flex flex-column">
           {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img src={playerDetails.image} alt="Player" style={{ width: '300px' }} />
+          <img src={playerDetails.image} alt="Player" style={{ width: '400px' }} />
         </div>
         <div className="ms-5 details">
-          <h1 className="text-white">{playerDetails.name}</h1>
-          <h3 className="text-white">{playerDetails.club_name}</h3>
-          <h5>{playerDetails.role}</h5>
+          <h1>{playerDetails.name}</h1>
+          <hr className="view-hr" />
+          <h3>{playerDetails.role}</h3>
         </div>
+      </div>
+      <div className="mt-4">
+        <Link href="/members/view" passHref>
+          <Button size="lg" className="btn-m">Go Back</Button>
+        </Link>
       </div>
     </>
   );
